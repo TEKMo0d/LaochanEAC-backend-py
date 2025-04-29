c875c1b66ef1f7d00b859cfb951b593a19f67a9e
kakenhi <101322626+TEKMo0d@users.noreply.github.com>
2025-04-29 18:15:45 +0800

    anyone helps me????????

--- tools/xml-to-json.py
+++ tools/xml-to-json.py
@@ -1,1 +1,26 @@
+from ..utils.kbinxml import toObject, XMLParser
+import json
 
+with open('input.xml','r') as file:
+    XML_CONTENT = file.read()
+    
+parserIntools = XMLParser({
+        "ignoreAttributes": False,
+        "parseAttributeValue": False,
+        "attributeNamePrefix": "$",
+        "removeNSPrefix": True,
+        "textNodeName": "__value",
+        "numberParseOptions": {
+            "hex": False,
+            "leadingZeros": False,
+            "eNotation": False,
+            "skipLike": r".*"
+        }
+    })
+
+parsed_xml = parserIntools.parse(XML_CONTENT)
+
+obj = toObject(parsed_xml)
+
+with open('output.json', 'w') as file:
+    json.dump(obj, file, indent=2)
